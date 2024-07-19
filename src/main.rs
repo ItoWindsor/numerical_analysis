555b2e606cae829a67cead3ae2815b0458f7f445
ItoWindsor <itowindsor@gmail.com>
2024-07-19 20:03:04 +0200

    update tests

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,20 @@ fn main() {
     let vec_val3 = vec![2,4,5,1,2,3];
     let nrows : u32 = 3;
     let ncols : u32 = 2;
-    
+  
+    let arr = [10,0];
+    println!("{:?}",arr);
     let mat1 : matrices::Matrix_t<i32> = matrices::Matrix_t::new_empty(nrows,ncols);
     let mat2 : matrices::Matrix_t<i32> = matrices::Matrix_t::new(nrows,ncols,vec_val);
     let mat3 : matrices::Matrix_t<i32> = matrices::Matrix_t::new(nrows,ncols,vec_val3);
-    println!("shape of the matrix : {:?}", mat1.get_shape());
-    println!("shape of the matrix : {:?}", mat2.get_shape());
-    println!("shape of the matrix : {:?}", mat3.get_shape());
-    for i in 0..nrows {
-        for j in 0..ncols {
-            println!("Mat[{}][{}] = {}", i,j, mat2[(i,j)]);
-        } 
-    }
-    println!("{}", mat1);
-    println!("{}", mat2);
-    println!("{}", mat3);
+    println!("mat1 = \n{}", mat1);
+    println!("mat2 = \n{}", mat2);
+    println!("mat3 = \n{}", mat3);
     println!("Sum of both : ");
-    println!("{}", mat2+mat3);
+    let mat_temp = &mat2 + &mat3;
+    let mat_temp2 = &mat3 + &mat2;
+    println!("{}", mat_temp); 
+    println!("{}", mat_temp2);
 
-    println!("{}", mat1);
-    println!("{}", mat2);
-    println!("{}", mat3);
+    println!("mat3 = \n{}", mat3); 
 }
