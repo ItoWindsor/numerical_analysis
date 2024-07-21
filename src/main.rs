4369d9855dfc1e437124b22551a2f346a9946e16
ItoWindsor <itowindsor@gmail.com>
2024-07-21 13:51:04 +0200

    update some tests -> I should think about having some REAL TESTS

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,15 @@ fn main() {
     let id_matrix2 : matrices::Matrix_t<i32> = matrices::Matrix_t::new_identity(3);
     
     let arr = [[0;3], [1;3], [2;3]];
-    let arr2 = [[0.1, 2.5, 5.6],[1.0;3]];
+    let arr2 = [[1.1, 2.5, 5.6],[1.0;3], [3.0;3]];
+    
     let mat_arr = matrices::Matrix_t::from_array(arr);
     let mat_arr2 = matrices::Matrix_t::from_array(arr2); 
-    let mat3 = &id_matrix1 * &id_matrix2;
-    let mat4 = &mat_arr * &mat_arr;
-    let mat5 = &mat_arr2 * &mat_arr;
-
-    println!("arr = {:?}", arr);
-    println!("arr2 = {:?}", arr2);
-    println!("mat from arr : \n{}",mat_arr);
-    println!("mat from arr2 : \n{}", mat_arr2);
-
-
-    println!("{}",mat3);
-    println!("{}",mat4);
-    println!("{}", mat5);
+    let mat_id : matrices::Matrix_t<i32> = matrices::Matrix_t::new_identity(3);
+    
+    let prod_mat2 = &mat_id * &mat_arr2;
+   
+    println!("{}", mat_id);
+    println!("{}",mat_arr2);
+    println!("{}", prod_mat2);
 }
